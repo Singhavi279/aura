73f988ac23e22ee7d8474d29c1a817c2fd2b13d9
AVNISH SINGH <t20avnish@gmail.com>
2020-08-25 21:22:53 +0530

    .

--- src/components/home/WhatWeDo.vue
+++ src/components/home/WhatWeDo.vue
@@ -6,7 +6,7 @@
         <p
           class="google-font mt-0"
           style="font-size:95%"
-        >Google Developer Groups (GDGs) are for developers who are interested in Google's developer technology.</p>
+        >Developer Student Clubs (DSCs) are for developers who are interested in Google's developer technology.</p>
         <p class="google-font" style="font-size:95%">About different Google technologies</p>
         <span v-for="(item,i) in whatwedodata" :key="i">
           <v-tooltip bottom>
